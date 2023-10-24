e037c5429a3a59aabe6cb1a4bbe70dc54b561df3
developer.namangor@gmail.com <developer.namangor@gmail.com>
2023-10-24 07:17:36 +0000

    Created AboutView, HomeBlogsFeed and BlogsView; Edited HomePage

--- client_code/HomePage.py
+++ client_code/HomePage.py
@@ -5,6 +5,10 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+from .HomeBlogsFeed import HomeBlogsFeed
+from .BlogsView import BlogsView
+from .AboutView import AboutView
+
 class HomePage(HomePageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -12,7 +16,39 @@ class HomePage(HomePageTemplate):
 
     # Any code you write here will run when the form opens.
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  def reset_links(self, **event_args):
+    self.homeLink.role = ''
+    self.blogsLink.role = ''
+    self.aboutLink.role = ''
+
+  def homeLink_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.reset_links()
+    self.homeLink.role = 'selected'
+    
+    self.content_panel.clear()
+    self.content_panel.add_component(HomeBlogsFeed())
     pass
+
+  def blogsLink_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.reset_links()
+    self.blogsLink.role = 'selected'
+    
+    self.content_panel.clear()
+    self.content_panel.add_component(BlogsView())
+    pass
+
+  def aboutLink_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.reset_links()
+    self.aboutLink.role = 'selected'
+    
+    self.content_panel.clear()
+    self.content_panel.add_component(AboutView())
+    pass
+
+
+
+
 
